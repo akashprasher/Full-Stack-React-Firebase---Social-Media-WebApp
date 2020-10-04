cf6d6e90f820a18b677edda34caf14fcfdc62bd0
akashprasher <prince.akash.prasher@gmail.com>
2020-10-04 12:56:35 +0530

    Removing function hello and adding getScream and creatScream

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,10 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.index = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 exports.getScreams = functions.https.onRequest((req, res) => {
     admin.firestore().collection('screams').get()
