12854c3ca2ad3d64054224176fedbaf72bf43fe3
akashprasher <prince.akash.prasher@gmail.com>
2020-10-07 10:45:37 +0530

    adding routes /getScream and /commentOnScream

--- functions/index.js
+++ functions/index.js
@@ -4,12 +4,30 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
+const {
+    getAllScreams,
+    postOneScream,
+    getScreams,
+    commentOnScream
+} = require('./handlers/screams');
+const {
+    signup,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser
+} = require('./handlers/users');
 
 // Scream routes
 app.get('/screams', getAllScreams);
-app.post('/scream', FBAuth, postOneScream)
+app.post('/scream', FBAuth, postOneScream);
+app.get('/screams/:screamId', getScreams);
+// TODO: Delete Scream
+// TODO: Like a scream
+// TODO: Unlike a scream
+// TODO: Comment on scream
+app.post('/screams/:screamId/comment', FBAuth, commentOnScream);
+
 
 // User routes
 app.post('/signup', signup);
