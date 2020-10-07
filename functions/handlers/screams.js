d4fbf90aa748dff441fe10cf5c02ced521894599
akashprasher <prince.akash.prasher@gmail.com>
2020-10-07 10:47:58 +0530

    defining fn for getComment and postComment

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -12,7 +12,7 @@ exports.getAllScreams = (req, res) => {
             let screams = [];
             data.forEach(doc => {
                 screams.push({
-                    screamID: doc.id,
+                    screamId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
                     createdAt: doc.data().createdAt
@@ -50,4 +50,56 @@ exports.postOneScream = (req, res) => {
             });
             console.error(err);
         })
+}
+
+// fetch comments of screams
+exports.getScreams = (req, res) => {
+    let screamData = {};
+    db.doc(`/screams/${req.params.screamId}`).get()
+    .then(doc => {
+        if(!doc.exists) return res.status(404).json({error: 'Scream not found'});
+        screamData = doc.data();
+        screamData.screamId = doc.id;
+        return db.collection('comments').orderBy('createdAt', 'desc').where('screamId', '==', req.params.screamId).get();
+    })
+    .then(data => {
+        screamData.comments = [];
+        data.forEach(doc => {
+            screamData.comments.push(doc.data())
+        });
+        return res.json(screamData);
+    })
+    .catch(err => {
+        console.error(err);
+        res.status(500).json({error: err.code});
+    })
+}
+
+// Comment on a scream
+exports.commentOnScream = (req, res) => {
+    if(req.body.body.trim() == "") return res.status(400).json({error: 'Must not be empty'});
+
+    const newComment = {
+        body: req.body.body,
+        createdAt: new Date().toISOString(),
+        screamId: req.params.screamId,
+        userHandle: req.user.handle,
+        userImage: req.user.imageUrl
+    };
+
+    db.doc(`/screams/${req.params.screamId}`).get()
+    .then(doc => {
+        if(!doc.exists) {
+            return res.status(404).json({error: "scream doesn't exists"});
+        }
+        return db.collection('comments').add(newComment);
+    })
+    .then(() => {
+        res.json({newComment});
+    })
+    .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: 'Something went wrong' });
+    })
+
 }
