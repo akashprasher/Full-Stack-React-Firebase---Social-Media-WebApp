254b08340a790835bc78df9b746c8ad64e0f80bd
akashprasher <prince.akash.prasher@gmail.com>
2020-10-12 19:37:54 +0530

    Applying beautify

--- functions/util/config.js
+++ functions/util/config.js
@@ -1,11 +1,11 @@
  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
  module.exports = {
-    apiKey: "AIzaSyBgqJfRtx7LI3HmqGpJEYnZvzJ8ahsr4Kc",
-    authDomain: "socialape-f6f64.firebaseapp.com",
-    databaseURL: "https://socialape-f6f64.firebaseio.com",
-    projectId: "socialape-f6f64",
-    storageBucket: "socialape-f6f64.appspot.com",
-    messagingSenderId: "197120470658",
-    appId: "1:197120470658:web:537afcea92ba17d12d8e9b",
-    measurementId: "G-1M8NHVELDM"
-  };
+   apiKey: "AIzaSyBgqJfRtx7LI3HmqGpJEYnZvzJ8ahsr4Kc",
+   authDomain: "socialape-f6f64.firebaseapp.com",
+   databaseURL: "https://socialape-f6f64.firebaseio.com",
+   projectId: "socialape-f6f64",
+   storageBucket: "socialape-f6f64.appspot.com",
+   messagingSenderId: "197120470658",
+   appId: "1:197120470658:web:537afcea92ba17d12d8e9b",
+   measurementId: "G-1M8NHVELDM"
+ };
